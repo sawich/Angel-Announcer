455c2f75ff0377343d2bfacb5562476f457c3b94
sawich <sawich@angeldev.org>
2018-07-18 00:46:15 +0300

    -

--- vk_observer.js
+++ vk_observer.js
@@ -9,6 +9,8 @@ module.exports = class vk_observer {
     this._group_user_lj_post = (body, msg = 'Подписочка', color = 0x00bfff) => {
       request (`https://api.vk.com/method/users.get?user_ids=${body.user_id}&fields=photo_50&lang=0&v=5.73`, { json: true })
       .then (res => {
+        console.log (res)
+        
         this.channel.log.send ({ embed: {
           color,
           description: `${msg} от [${res.response.first_name} ${res.response.last_name}](https://vk.com/id${res.response.id})`,
@@ -32,7 +34,7 @@ module.exports = class vk_observer {
     this._group_user_lj_post(body, 'Отписочка', 0xffff00)    
   }
 
-  async group_join() {
-    this._group_user_lj_post()
+  async group_join(body) {
+    this._group_user_lj_post(body)
   }
 }
