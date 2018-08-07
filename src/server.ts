20f98dbeb504fe87ae6e7117bbd9a4b388aa8b3d
sawich <sawich@angeldev.org>
2018-08-07 06:43:11 +0300

    -

--- src/server.ts
+++ src/server.ts
@@ -429,7 +429,10 @@ class CApp {
 							
 							for(const page of data.pages) {
 								const current_page = `***[#](${page.url})***`
+								
 								const page_fields = page.comments.map(comment => `${current_page} \`\`[${comment.datetime}]\`\` ***[${comment.author.replace(/\*/g, '\◘')}](${comment.author_link})*** : ${comment.message.replace(/\*/g, '\▼').replace(/_{2,}/g, '○')}\n`)
+								if(!page_fields.length) { continue }
+
 								const current_page_title = `— стр. ${1 + page.page_id} —`
 								const embed_default_size = data.name.length + current_page_title.length
 					
